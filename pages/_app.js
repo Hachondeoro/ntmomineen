dcfb2a941a64adb7576de3f9dda8783017ea6be7
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2021-08-05 20:34:31 +0930

    added google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "swiper/swiper-bundle.min.css";
+import App from "next/app";
+import React from "react";
+import TagManager from "react-gtm-module";
 import "react-modal-video/css/modal-video.min.css";
+import "swiper/swiper-bundle.min.css";
 import "../assets/css/animate.min.css";
 import "../assets/css/azino-icons.css";
+import "../assets/css/custom.scss";
 import "../assets/css/fontawesome-all.min.css";
 import "../assets/css/main.css";
-import "swiper/swiper-bundle.min.css";
-import "../assets/css/custom.scss";
-import '../assets/css/react-tabs.css'
+import "../assets/css/react-tabs.css";
 
+export default class MyApp extends App {
+  componentDidMount() {
+    TagManager.initialize({
+      gtmId: "GTM-PM3WHZK",
+    });
+  }
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
