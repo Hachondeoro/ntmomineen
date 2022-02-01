db4713f6d0956b3b3c6a852a555f861ca28f82ef
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2022-02-02 00:14:05 +0930

    Implemented Islamic Calendar

--- components/services/service-one.js
+++ components/services/service-one.js
@@ -1,14 +1,32 @@
 import EventCard from "@components/event/event-card";
 import islamicCalendar from "public/shiacalendar2.pdf";
 import React from "react";
-import { Container } from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import serviceBg from "../../assets/images/backgrounds/service-hand-bg-1-1.png";
 import serviceLine from "../../assets/images/shapes/service-line-1-1.png";
 
 import BTable from "react-bootstrap/Table";
-import { useTable } from "react-table";
+import {useTable} from "react-table";
+import {PrayTimes} from "../../assets/PrayTimes";
 
 const ServiceOne = (props) => {
+  var prayTimes = new PrayTimes();
+  prayTimes.setMethod("Jafari");
+  var date = new Date(); // today
+  var times_clock = prayTimes.getTimes(date, [-12.476632, 130.987125], 10, 1);
+  var list_times = ["Fajr", "Sunrise", "Dhuhr", "Sunset", "Maghrib"];
+  var list_times_lowercase = list_times.map((v) => v.toLowerCase()); // to filter times returned
+  const full_list = list_times_lowercase.reduce((obj, key) => ({ ...obj, [key]: times_clock[key] }), {});
+  const result = list_times.map((val, i) => ({ period: val, time: Object.values(full_list)[i] })); // for the table
+
+  const data_prayers = React.useMemo(() => result, []);
+  const columns_prayers = React.useMemo(
+    () => [
+      { Header: "Period", accessor: "period" },
+      { Header: "Time", accessor: "time" },
+    ],
+    [],
+  );
   const data = React.useMemo(() => props.events.allUpcomingEvents, []);
   // prettier-ignore
   const columns = React.useMemo(
@@ -58,18 +76,30 @@ const ServiceOne = (props) => {
     <section className="service-one pt-120 pb-10" style={{ backgroundImage: `url(${serviceBg})` }}>
       <Container>
         <img src={serviceLine} alt="" className="service-one__shape-1" />
-        <div className="block-title">
-          <a
-            className="scroll-to-target thm-btn"
-            data-target=".donate-options"
-            href={islamicCalendar}
-            target="_blank">
-            Download Yearly Islamic Calendar 1442A.H.
-          </a>
-        </div>
+        <Row>
+          <Col sm={6}>
+            <div className="block-title">
+              <a
+                className="scroll-to-target thm-btn"
+                data-target=".donate-options"
+                href={islamicCalendar}
+                target="_blank">
+                Download Yearly Islamic Calendar 1442A.H.
+              </a>
+            </div>
+          </Col>
+          <Col sm={6}>
+            <div>
+              <h2>Prayer Time - Darwin</h2>
+              <div style={{ fontSize: "0.7em" }}>
+                <Table columns={columns_prayers} data={data_prayers} />
+              </div>
+            </div>
+          </Col>
+        </Row>
         <div className="block-title">
           <h3>Upcoming events</h3>
-          <div style={{fontSize:"0.7em"}}>
+          <div style={{ fontSize: "0.7em" }}>
             <Table columns={columns} data={data} />
           </div>
           <section className="event-page pt-20">
--- components/videos/video-card-two.js
+++ components/videos/video-card-two.js
@@ -11,18 +11,12 @@ const VideoCardTwo = () => {
   useEffect(() => {}, []);
   return (
     <section className="video-card-two">
-      <ModalVideo
-        channel="youtube"
-        autoplay
-        isOpen={isOpen}
-        videoId="wgP_OSOS3IA"
-        onClose={() => setOpen(false)}
-      />
+      <ModalVideo channel="youtube" autoplay isOpen={isOpen} videoId="wgP_OSOS3IA" onClose={() => setOpen(false)} />
       <Container>
         <div className="inner-container" style={{ backgroundImage: `url(${videoBg})` }}>
           <Row className="align-items-center">
             <Col lg={3}>
-              <img src={videoImage} alt="" width="200px"/>
+              <img src={videoImage} alt="" width="200px" />
             </Col>
             {/* <Col lg={4} className="ml-auto mr-auto text-center">
               <iframe
@@ -36,11 +30,10 @@ const VideoCardTwo = () => {
             </Col> */}
             <Col lg={9}>
               <p>
-                Northern Territory Momineen Incorporated (NTMI) is a non-profit registered
-                organization of the Shia Ithna Asheri Muslim community of Darwin, Australia. <br />{" "}
-                It is serving the religious and educational needs of over 100 members from
-                communities of India, Pakistan, East Africa, Singapore, Afghanistan, Lebanon, Iraq,
-                Syria and Iran.
+                Northern Territory Momineen Incorporated (NTMI) is a non-profit registered organization of the Shia
+                Ithna Asheri Muslim community of Darwin, Australia. <br /> It is serving the religious and educational
+                needs of over 100 members from communities of India, Pakistan, East Africa, Singapore, Afghanistan,
+                Lebanon, Iraq, Syria and Iran.
               </p>
               <br />
             </Col>
